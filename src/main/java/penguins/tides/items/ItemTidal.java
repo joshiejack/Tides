5b93d4e001fd8bde97642cfa5d7ae6d76da2708c
Yulife <YulifeGaming@t-online.de>
2016-08-05 15:44:27 +0200

    Item render doesn't work @joshiejack
    we should store pearls in variants or metas or nbt

--- src/main/java/penguins/tides/items/ItemTidal.java
+++ src/main/java/penguins/tides/items/ItemTidal.java
@@ -3,6 +3,7 @@ package penguins.tides.items;
 import net.minecraft.item.Item;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.common.registry.GameRegistry;
+import penguins.tides.Tides;
 
 import static penguins.tides.lib.TidesInfo.MODID;
 
@@ -10,6 +11,7 @@ public class ItemTidal extends Item {
     public Item register(String name) {
         setUnlocalizedName(name);
         setRegistryName(new ResourceLocation(MODID, name));
+        setCreativeTab(Tides.TAB); //Default out the tab
         GameRegistry.register(this);
         return this;
     }
